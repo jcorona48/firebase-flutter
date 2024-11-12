558d5378c33a88ee18f1596cd966d69a9a9c0a68
Joan Corona <joaneliasc86@gmail.com>
2024-11-11 22:05:53 -0400

    feat: ✨ add image asset to HomeScreen for enhanced visual appeal

--- lib/home.dart
+++ lib/home.dart
@@ -24,6 +24,16 @@ class HomeScreen extends StatelessWidget {
                         Navigator.of(context).pop();
                       })
                     ],
+                    children: [
+                      const Divider(),
+                      Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Image.asset('assets/flutterfire_300x.png'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               );
